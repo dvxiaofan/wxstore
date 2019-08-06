ded7706aba9e3b10d999d16638fdfbf74be20a38
xiaofan <1353815633@qq.com>
2019-08-06 13:28:57 +0800

    搭建商品详情页

---
+++ miniprogram/pages/detail/detail.js
@@ -0,0 +1,78 @@
+/*
+ * @Author: zhang 
+ * @Date: 2019-08-06 13:20:51 
+ * @Last Modified by:   zhang 
+ * @Last Modified time: 2019-08-06 13:20:51 
+ */
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    product: {
+      id: 2,
+      image: 'http://placekitten.com/300/300',
+      name: 'Guitar',
+      price: 399.80,
+      source: 'SWEDEN'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
