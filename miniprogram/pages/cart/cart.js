593b47666803cb790b0a1e1a8bbc4bca36f5fa5a
xiaofan <1353815633@qq.com>
2019-08-09 13:38:03 +0800

    购物车静态页面

--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -2,7 +2,7 @@
  * @Author: zhang 
  * @Date: 2019-08-07 13:55:55 
  * @Last Modified by: zhang
- * @Last Modified time: 2019-08-09 10:39:55
+ * @Last Modified time: 2019-08-09 13:28:50
  */
 
 
@@ -14,7 +14,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: null,
+    cartList: [{
+      id: 1,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+      name: 'Wallet',
+      price: '100.00',
+      source: 'CHINA',
+      count: 1,
+    }, {
+      id: 2,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
+      name: 'Guitar',
+      price: '200.00',
+      source: 'SWEDEN',
+      count: 3,
+    }, {
+      id: 3,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product3.jpg',
+      name: 'Stapler',
+      price: '300.00',
+      source: 'GERMANY',
+      count: 4,
+    }, {
+      id: 4,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product4.jpg',
+      name: 'Leafy vegetables',
+      price: '400.00',
+      source: 'NEW ZEALAND',
+      count: 2,
+    }, {
+      id: 5,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product5.jpg',
+      name: 'Compass',
+      price: '500.00',
+      source: 'USA',
+      count: 1,
+    }],
+    isSelectAllChecked: false,
+    isCartEdit: false,
+    cartCheckMap: {},
+    cartTotal: '45.99'
   },
 
   /**
